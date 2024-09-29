6dde5a0a62fd6323e77c338e2790a9dd5f8a90d0
Swastika47 <swastikabanerjee0407@gmail.com>
2024-09-29 16:37:13 +0530

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-let currentIndex = 0;
-
-function changeSlide(direction) {
-    const slides = document.querySelector('.slides');
-    const totalImages = slides.children.length;
-    currentIndex = (currentIndex + direction + totalImages) % totalImages;
-    const width = slides.children[0].clientWidth;
-    slides.style.transform = `translateX(-${currentIndex * width}px)`;
-}
